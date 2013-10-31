ffdb55012d313f3d778bae72a249e032b1503bc7
Nils Naumann <nau@gmx.net>
2013-10-31 18:02:33 +0100

    postrm erstellt

--- debian/fritzident.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fritzident package
-#
-0 4	* * *	root	[ -x /usr/bin/fritzident_maintenance ] && /usr/bin/fritzident_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fritzident):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="fritzident" command="/usr/bin/fritzident"

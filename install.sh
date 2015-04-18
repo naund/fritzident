ffdf724447ce55ae0f4cd26eb9b86a150c5e1de1
kun. Jonas <jsrjenkins@gmail.com>
2015-04-18 07:10:22 +0200

    Corrected spelling error in install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #
 apt-get install openbsd-inetd update-inetd logrotate
 update-inetd --remove 14013
-install fritzident /usr/local/sbin/firtzident
+install fritzident /usr/local/sbin/fritzident
 install logrotate.fritzident /etc/logrotate.d/fritzident
 update-inetd --add "14013	stream	tcp	nowait	root	/usr/local/sbin/fritzident fritzindent -l /var/log/fritzident.log"

d60c34ba72ac990dcc3d5b652c107b6800345c72
kun. Jonas <jsrjenkins@gmail.com>
2015-04-18 07:20:50 +0200

    adding make to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #
 apt-get install openbsd-inetd update-inetd logrotate
 update-inetd --remove 14013
+make
 install fritzident /usr/local/sbin/fritzident
 install debian/fritzident.logrotate /etc/logrotate.d/fritzident
 update-inetd --add "14013	stream	tcp	nowait	root	/usr/local/sbin/fritzident fritzindent -l /var/log/fritzident.log"

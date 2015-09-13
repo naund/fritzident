6afcf8a8a2e8031aac76633ad355a655f667d7bf
Nils Naumann <nau@gmx.net>
2015-09-13 19:25:00 +0200

    Add install rules to Makefile

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-apt-get install openbsd-inetd update-inetd logrotate
-update-inetd --remove 14013
-make
-install fritzident /usr/local/sbin/fritzident
-install debian/fritzident.logrotate /etc/logrotate.d/fritzident
-update-inetd --add "14013	stream	tcp	nowait	root	/usr/local/sbin/fritzident fritzindent -l /var/log/fritzident.log"
